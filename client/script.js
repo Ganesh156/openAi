10174f24787261949057c526ad965ad93c2aac16
Ganesh Hegde <ganesh_hegde@Dineroai.com>
2022-12-28 11:48:33 +0530

    server connection done

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://coder-tpqo.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
